a1994ca018980bd82ec36eae1d8c24e26824158e
Kayra Czerniewicz <kayracz@gmail.com>
2019-11-29 00:13:03 -0400

    heroku deployment test 2

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,9 @@ class PagesController < ApplicationController
   def index
     ufs = UserFollower.where(user_id: session[:user_id]) #user_id, follower_id
     follower_ids = ufs.map{|u| u.follower_id} # follower_id
-    @followers = Follower.where(id: follower_ids) #
+    @followers = Follower.where(id: follower_ids)
 
     if @followers.blank?
-      binding.pry
       twitter_response = TwitterAPI.new
       @followers ||= twitter_response.client.followers
       @followers.each do |follower|
@@ -13,7 +12,6 @@ class PagesController < ApplicationController
         UserFollower.create(user_id: session[:user_id], follower_id: f.id)
       end
     end
-
 
     # User have many followers
     # Follower have many users
