731502f383cffe357a6924f4281c728c940e1c61
Kayra Czerniewicz <kayracz@gmail.com>
2019-11-29 16:05:24 -0400

    heroku test 4

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,21 +4,16 @@ class PagesController < ApplicationController
     follower_ids = ufs.map{|u| u.follower_id} # follower_id
     @followers = Follower.where(id: follower_ids)
 
+    puts 'followers before API', @followers, @followers.blank?
     if @followers.blank?
       twitter_response = TwitterAPI.new
       @followers ||= twitter_response.client.followers
+      puts 'after API call', @followers
       @followers.each do |follower|
         f = Follower.find_or_create_by(name: follower.name, user_id: follower.id)
         UserFollower.create(user_id: session[:user_id], follower_id: f.id)
       end
     end
-
-    # User have many followers
-    # Follower have many users
-    # rails g model Follower name:string
-
-    # USserFollower join table
-    # rails g model UserFollower user:references follower:references
-    # @followers = UserFollower.find(user: current_user).followers
   end
 end
+
