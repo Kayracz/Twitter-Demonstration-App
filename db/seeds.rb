7fa826fb27afe1127fa530a03e761ab80c5a9f63
Kayra Czerniewicz <kayracz@gmail.com>
2019-11-29 12:48:56 -0400

    heroku test 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+#   Character.create(name: 'Luke', movie: movies.first)
+unless Product.count > 0
+    Product.create!(
+      title: "Hoodie", description: "An awesome hoodie!", price: 23.45,
+      photo: "http://res.cloudinary.com/deruwllkv/image/upload/v1565141088/a6usehtmcxhskup7cmex.png",
+      size: "L",
+      color: "Red",
+      category: "men")
+end
